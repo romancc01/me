74104de5fbb76b8d06cdf7cdaf33945ce16e6c06
romancc01 <romancc01@gmail.com>
2023-04-27 17:06:02 -0600

    Cambios diseño footer

--- src/App.js
+++ src/App.js
@@ -7,7 +7,10 @@ import Granportafolio from './components/granportfolio'
 import Portfoliothumbs from './components/portfoliothumbs'
 import MenuP from './components/menuP'
 import { useState } from 'react';
-
+import { CssBaseline } from '@mui/material';
+import "@fontsource/roboto";
+import { RiLinkedinBoxFill} from "react-icons/ri";
+import Container from '@mui/material/Container';
 
 import * as React from 'react';
 
@@ -18,20 +21,43 @@ function App() {
   const handleImageClick = (imagen) => {
     setImagenSeleccionada(imagen);
   };
-  return (
-    <div className="App">
-      <header>
-        <MenuP/>
-      </header>
-      <main>
-        <div style={{ display: 'flex', flexWrap: 'nowrap' }}>
+//   return (
+
+//     <><Container className="App">
+//       <CssBaseline />
+//       <header>
+//         <MenuP />
+//       </header>
+//       <main>
+//         <section style={{ display: 'flex', flexWrap: 'nowrap' }}>
+//           <Granportafolio imagenSeleccionada={imagenSeleccionada} />
+//           <Portfoliothumbs onImageClick={handleImageClick} />
+//         </section>
+//       </main>
+//     </Container>
+//     <footer>Román Contreras <RiLinkedinBoxFill/></footer></>
+
+//   );
+// }
+
+// export default App;
+return (
+  <div className="app-container">
+    <CssBaseline />
+    <header>
+      <MenuP />
+    </header>
+    <main>
+      <Container>
+        <section style={{ display: 'flex', flexWrap: 'nowrap' }}>
           <Granportafolio imagenSeleccionada={imagenSeleccionada} />
           <Portfoliothumbs onImageClick={handleImageClick} />
-          </div>
-      </main>
-      <footer>Footer</footer>
+        </section>
+      </Container>
+    </main>
+    <footer>Román Contreras <RiLinkedinBoxFill src="https://www.linkedin.com/in/rom%C3%A1n-contreras-campillo-79a15375/"/></footer>
   </div>
-  );
+);
 }
 
 export default App;
